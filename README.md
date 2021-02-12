f68966c25e65cd09a5394687db51f4f0ce6cc401
Nikilauz <myinternetid@gmx.de>
2021-02-12 16:22:06 +0100

    comments, arg + svg functionality, if statement

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # sysmon.sh
-A simple system monitoring script to set as source of [xfce4 gemon panel plugin](https://gitlab.xfce.org/panel-plugins/xfce4-genmon-plugin).
+A simple system monitoring script for xfce desktops to set as source of [xfce4 gemon panel plugin](https://gitlab.xfce.org/panel-plugins/xfce4-genmon-plugin).
 It uses some of the sample scripts provided by genmon.
--- sysmon.sh
+++ sysmon.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-network_adapter=wlp2s0
+# args
+network_adapter=$(echo $@ | grep -E -e "-a .+" | awk '{printf("%s", $2)}')
+as_svg=`if [[ $(echo $@ | grep -e "--svg") ]]; then echo 1; else echo 0; fi`
 
+# script location directory
 script_dir=$(dirname ${BASH_SOURCE[0]})
 
+function svg_bar() {
+    svg+="<rect x=\"$1\" y=\""$(( 100 - $2 ))"%\" width=\"10\" height=\"$2%\" fill=\"$3\" />"
+    svg+="<rect x=\"$1\" width=\"10\" height=\"100%\" fill=\"none\" stroke=\"black\" stroke-width=\"1\" />"
+}
+
+
+# get values
 
 # https://gitlab.xfce.org/panel-plugins/xfce4-genmon-plugin/-/blob/master/scripts/sysstat
-cpu=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{printf("%2d", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5))}')
+cpu=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{printf("%d", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5))}')
 
 gpu=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
 
@@ -16,7 +26,28 @@ swap=$(free | sed '3q;d' | awk '{printf("%d",  $3/$2 * 100.0)}')
 
 network=$($script_dir/scripts_monBandwidth $network_adapter)
 
-echo "<txt> <b>v/^</b> $network <b>C</b> $cpu% <b>G</b> $gpu% <b>R</b> $ram% <b>S</b> $swap% </txt>"
+# create textual output
+text_out="<b>Network (v/^):</b> $network <b>CPU:</b> $cpu% <b>GPU:</b> $gpu% <b>RAM:</b> $ram% <b>swap:</b> $swap%"
 
-#/home/julian/Code/bash/sysmon/sysmon.sh
-
+# create svg if requested
+if (( $as_svg )); then
+    color=$(if [[ $(xfconf-query -lvc xsettings -p /Net/ThemeName | grep -i "dark") ]]; then echo white; else echo black; fi)
+    font=$(xfconf-query -lvc xsettings -p /Gtk/FontName | awk '{printf("%s", $2)}')
+    svg="<svg width=\"120\" height=\"30\">"
+    svg+="<g fill=\"$color\" font-size=\"12\" font-family=\"$font\">"
+    svg+="<text x=\"0\" y=\"15\" textLength=\"50\" lengthAdjust=\"spacingAndGlyphs\">"
+    svg+="<tspan>&#8593;"$(echo $network | awk '{printf("%s %s", $3, $4)}')"</tspan>"
+    svg+="<tspan x=\"0\" dy=\"13\">&#8595;"$(echo $network | awk '{printf("%s %s", $1, $2)}')"</tspan>"
+    svg+="</text>"
+    svg_bar 60 $cpu blue
+    svg_bar 75 $gpu green
+    svg_bar 90 $ram orange
+    svg_bar 105 $swap yellow
+    svg+="</g>"
+    svg+="</svg>"
+    
+    echo $svg > $script_dir/icon.svg
+    echo "<img>$script_dir/icon.svg</img><tool>$text_out</tool>"
+else
+    echo "<txt> $text_out </txt>"
+fi

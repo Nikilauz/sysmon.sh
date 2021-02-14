72e1d9d2a59c9f23dec12212f8efc24ec28b099b
Nikilauz <myinternetid@gmx.de>
2021-02-14 12:06:49 +0100

    merge scripts, add no-gpu, refactoring, readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sysmon.sh
 A simple system monitoring script for xfce desktops to set as source of [xfce4 genmon panel plugin](https://gitlab.xfce.org/panel-plugins/xfce4-genmon-plugin).  
-It uses some of the sample scripts provided by genmon.
+It uses some of the [sample scripts](https://gitlab.xfce.org/panel-plugins/xfce4-genmon-plugin/-/blob/master/scripts/) provided by genmon (sysstat, monBandwidth).
 #### setup
 1. download this repo (e.g. to ~/.local/share/sysmon)
 2. install [xfce4-gemon-plugin](apt://xfce4-genmon-plugin)
@@ -8,8 +8,9 @@ It uses some of the sample scripts provided by genmon.
 4. right-click on the newly added item in your panel and set the command path property to your `sysmon.sh` script
 #### options
  - `-a [network interface]` choose a network interface (e.g. via executing `netstat -i`), `eth0` is used if not provided
- - `--no-svg` only textual output
+ - `--text` only textual output, no bars
+ - `--no-gpu` disables gpu monitoring
 #### faq
  - Hover the panel item to see the order of the displayed values.
- - Currently, only GPU-Utilization monitoring Nvidia-GPU's via `nvidia-smi` is supported, please change the script to your needs or wait for updates to come...
+ - Currently, GPU-Utilization monitoring is supported for Nvidia via `nvidia-smi` (ships with proprietary drivers), for AMD (same here) via `aticonfig` and for Intel via `intel-gpu-tools` (make sure you have installed that one).
  
--- sysmon.sh
+++ sysmon.sh
@@ -1,48 +1,101 @@
 #!/bin/bash
 
+
+# get gpu load
+function get_gpu() {
+    if [[ `which nvidia-smi` ]]; then
+        echo `nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits`
+    elif [[ `which aticonfig --odgc --odgt` ]]; then
+        echo `aticonfig --odgc --odgt | grep "GPU load" | awk '{printf("%s", $4)}'`
+    elif [[ `which intel_gpu_top` ]]; then
+        intel=`(timeout 0.3 intel_gpu_top)`
+        echo $(echo `echo $intel | grep "render busy" | cut -d "%:" -f3\
+            ` `echo $intel | grep "bitstream busy" | cut -d ":" -f3\
+            ` `echo $intel | grep "blitter busy" | cut -d ":" -f3`\
+            | awk '{printf("%d\%", ($1+$2+$3)/3)}')
+    else
+        echo "no supported GPU (driver)"
+    fi
+}
+
+# print svg bar; params: x-position amplitude fill-color
+function svg_bar() {
+    echo "<rect x=\"$1\" y=\""$(( 100 - $2 ))"%\" width=\"10\" height=\"$2%\" fill=\"$3\" />"
+    echo "<rect x=\"$1\" width=\"10\" height=\"100%\" fill=\"none\" stroke=\"black\" stroke-width=\"1\" />"
+}
+
+# format bit rate; params: bit-rate
+human_bandwidth () {
+    bandwidth=$1
+    p=0
+    while [ "$bandwidth" -gt "1024" -a "$p" -le "3" ] ; do
+        bandwidth=$(($bandwidth/1024))
+        p=$(($p+1))
+    done
+    case $p in
+        0)
+        bandwidth="$bandwidth B/s"
+        ;;
+        1)
+        bandwidth="$bandwidth KB/s"
+        ;;
+        2)
+        bandwidth="$bandwidth MB/s"
+        ;;
+    esac
+    echo $bandwidth
+}
+
+
 # args
 network_adapter=$(echo $@ | grep -E -e "-a .+" | awk '{printf("%s", $2)}')
-as_svg=`if [[ $(echo $@ | grep -e "--no-svg") ]]; then echo 0; else echo 1; fi`
+network_adapter=`(test $network_adapter && echo $network_adapter) || echo eth0`
+network=`test -d /sys/class/net/$network_adapter && echo 1`
+text=$(echo $@ | grep -c -e "--text")
+no_gpu=$(echo $@ | grep -e "--no-gpu")
 
 # script location directory
 script_dir=$(dirname ${BASH_SOURCE[0]})
 
-function svg_bar() {
-    svg+="<rect x=\"$1\" y=\""$(( 100 - $2 ))"%\" width=\"10\" height=\"$2%\" fill=\"$3\" />"
-    svg+="<rect x=\"$1\" width=\"10\" height=\"100%\" fill=\"none\" stroke=\"black\" stroke-width=\"1\" />"
-}
-
 
 # get values
 
-# https://gitlab.xfce.org/panel-plugins/xfce4-genmon-plugin/-/blob/master/scripts/sysstat
-cpu=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{printf("%d", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5))}')
-
-gpu=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
-
+# staticly
 ram=$(free | sed '2q;d' | awk '{printf("%d",  $3/$2 * 100.0)}')
-
 swap=$(free | sed '3q;d' | awk '{printf("%d",  $3/$2 * 100.0)}')
+gpu=$(test $no_gpu || get_gpu)
 
-network=$($script_dir/scripts_monBandwidth $network_adapter)
+# progressively
+cpu1=$(cat <(grep 'cpu ' /proc/stat))
+rx1=`test $network && cat /sys/class/net/$network_adapter/statistics/rx_bytes`
+tx1=`test $network && cat /sys/class/net/$network_adapter/statistics/tx_bytes`
+sleep 1
+cpu2=$(cat <(grep 'cpu ' /proc/stat))
+rx2=`test $network && cat /sys/class/net/$network_adapter/statistics/rx_bytes`
+tx2=`test $network && cat /sys/class/net/$network_adapter/statistics/tx_bytes`
+
+cpu=$(echo $cpu1 $cpu2 | awk -v RS="" '{printf("%d", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5))}')
+rx=`test $network && human_bandwidth $(($rx2 - $rx1))`
+tx=`test $network && human_bandwidth $(($tx2 - $tx1))`
+
 
 # create textual output
-text_out="<b>Network (v/^):</b> $network <b>CPU:</b> $cpu% <b>GPU:</b> $gpu% <b>RAM:</b> $ram% <b>swap:</b> $swap%"
+text_out="<b>Network:</b> v$tx ^$rx <b>CPU:</b> $cpu% <b>GPU:</b> $gpu% <b>RAM:</b> $ram% <b>swap:</b> $swap%"
 
 # create svg if requested
-if (( $as_svg )); then
+if ! (( $text )); then
     color=$(if [[ $(xfconf-query -lvc xsettings -p /Net/ThemeName | grep -i "dark") ]]; then echo white; else echo black; fi)
     font=$(xfconf-query -lvc xsettings -p /Gtk/FontName | awk '{printf("%s", $2)}')
+
     svg="<svg width=\"120\" height=\"30\">"
     svg+="<g fill=\"$color\" font-size=\"12\" font-family=\"$font\">"
     svg+="<text x=\"0\" y=\"15\" textLength=\"50\" lengthAdjust=\"spacingAndGlyphs\">"
-    svg+="<tspan>&#8593;"$(echo $network | awk '{printf("%s %s", $3, $4)}')"</tspan>"
-    svg+="<tspan x=\"0\" dy=\"13\">&#8595;"$(echo $network | awk '{printf("%s %s", $1, $2)}')"</tspan>"
+    svg+="<tspan>&#8593;"${tx:-#}"</tspan><tspan x=\"0\" dy=\"13\">&#8595;"${rx:-#}"</tspan>"
     svg+="</text>"
-    svg_bar 60 $cpu blue
-    svg_bar 75 $gpu green
-    svg_bar 90 $ram orange
-    svg_bar 105 $swap yellow
+    svg+=`svg_bar 60 $cpu blue`
+    svg+=`test $no_gpu || svg_bar 75 $gpu green`
+    svg+=`svg_bar 90 $ram orange`
+    svg+=`svg_bar 105 $swap yellow`
     svg+="</g>"
     svg+="</svg>"
     
